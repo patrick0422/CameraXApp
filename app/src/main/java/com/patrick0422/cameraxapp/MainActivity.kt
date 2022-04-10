793f8c17d5f728bcf2633f77727b98fa08f196a3
양태웅 <didxodnd0422@gmail.com>
2022-04-10 18:35:35 +0900

    Implement ImageCapture

--- app/src/main/java/com/patrick0422/cameraxapp/MainActivity.kt
+++ app/src/main/java/com/patrick0422/cameraxapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.patrick0422.cameraxapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.Manifest
+import android.content.ContentValues
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -12,9 +13,14 @@ import androidx.camera.video.*
 import androidx.camera.video.VideoCapture
 import java.util.concurrent.ExecutorService
 import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.databinding.DataBindingUtil
 import com.patrick0422.cameraxapp.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 typealias LumaListener = (luma: Double) -> Unit
 
@@ -30,25 +36,96 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Request camera permissions
+        // 권한 요청
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        // Set up the listeners for take photo and video capture buttons
         binding.imageCaptureButton.setOnClickListener { takePhoto() }
         binding.videoCaptureButton.setOnClickListener { captureVideo() }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    private fun takePhoto() {}
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg") // MIME = Multipurpose Internet Mail Extensions, https://server-talk.tistory.com/183 참조
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
 
     private fun captureVideo() {}
 
-    private fun startCamera() {}
+    private fun startCamera() {
+        /** ProcessCameraProvider가 Lifecycle을 인식할 수 있기 때문에 카메라를 따로 열고 닫는 작업이 필요없음 (메모리 누수 방지?) */
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(
+            {
+                //카메라 생명주기를 동기화 하는데 사용
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+                // 미리보기 구성
+                val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                    }
+
+                imageCapture = ImageCapture.Builder().build()
+
+                // 후면 카메라를 기본으로 선택
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                try {
+                    // cameraProvider와 연결된 모든 것을 끊음
+                    cameraProvider.unbindAll()
+                    // 현재 액티비티의 생명주기에 동기화
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+                } catch (e: Exception) {
+                    Log.e(TAG, "Use case binding failed", e)
+                }
+            },
+            ContextCompat.getMainExecutor(this) // UI 쓰레드의 Executor
+        )
+    }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
