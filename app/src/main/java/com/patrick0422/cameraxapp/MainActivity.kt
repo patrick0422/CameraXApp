5aed5c8dc75b1a0b7b0907f472428395f5bebb9d
양태웅 <didxodnd0422@gmail.com>
2022-04-10 18:57:21 +0900

    Implement VideoCapture

--- app/src/main/java/com/patrick0422/cameraxapp/MainActivity.kt
+++ app/src/main/java/com/patrick0422/cameraxapp/MainActivity.kt
@@ -17,6 +17,7 @@ import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.PermissionChecker
 import androidx.databinding.DataBindingUtil
 import com.patrick0422.cameraxapp.databinding.ActivityMainBinding
 import java.nio.ByteBuffer
@@ -92,7 +93,73 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun captureVideo() {}
+    private fun captureVideo() {
+        val videoCapture = this.videoCapture ?: return
+
+        binding.videoCaptureButton.isEnabled = false
+
+        val curRecording = recording
+        if (curRecording != null) {
+            // Stop the current recording session.
+            curRecording.stop()
+            recording = null
+            return
+        }
+
+        // create and start a new recording session
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
+            }
+        }
+
+        val mediaStoreOutputOptions = MediaStoreOutputOptions
+            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+        recording = videoCapture.output
+            .prepareRecording(this, mediaStoreOutputOptions)
+            .apply {
+                if (PermissionChecker.checkSelfPermission(this@MainActivity,
+                        Manifest.permission.RECORD_AUDIO) ==
+                    PermissionChecker.PERMISSION_GRANTED)
+                {
+                    withAudioEnabled()
+                }
+            }
+            .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+                when(recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        binding.videoCaptureButton.apply {
+                            text = getString(R.string.stop_capture)
+                            isEnabled = true
+                        }
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        if (!recordEvent.hasError()) {
+                            val msg = "Video capture succeeded: " +
+                                    "${recordEvent.outputResults.outputUri}"
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT)
+                                .show()
+                            Log.d(TAG, msg)
+                        } else {
+                            recording?.close()
+                            recording = null
+                            Log.e(TAG, "Video capture ends with error: " +
+                                    "${recordEvent.error}")
+                        }
+                        binding.videoCaptureButton.apply {
+                            text = getString(R.string.start_capture)
+                            isEnabled = true
+                        }
+                    }
+                }
+            }
+    }
 
     private fun startCamera() {
         /** ProcessCameraProvider가 Lifecycle을 인식할 수 있기 때문에 카메라를 따로 열고 닫는 작업이 필요없음 (메모리 누수 방지?) */
@@ -110,15 +177,20 @@ class MainActivity : AppCompatActivity() {
                         it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
                     }
 
+                val recorder = Recorder.Builder()
+                    .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
+                    .build()
+                videoCapture = VideoCapture.withOutput(recorder)
+
                 imageCapture = ImageCapture.Builder().build()
 
-                val imageAnalyzer = ImageAnalysis.Builder()
-                    .build()
-                    .also {
-                        it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                            Log.d(TAG, "Average luminosity: $luma")
-                        })
-                    }
+//                val imageAnalyzer = ImageAnalysis.Builder()
+//                    .build()
+//                    .also {
+//                        it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+//                            Log.d(TAG, "Average luminosity: $luma")
+//                        })
+//                    }
 
                 // 후면 카메라를 기본으로 선택
                 val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
@@ -127,7 +199,7 @@ class MainActivity : AppCompatActivity() {
                     // cameraProvider와 연결된 모든 것을 끊음
                     cameraProvider.unbindAll()
                     // 현재 액티비티의 생명주기에 동기화
-                    cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalyzer)
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, videoCapture)
                 } catch (e: Exception) {
                     Log.e(TAG, "Use case binding failed", e)
                 }
